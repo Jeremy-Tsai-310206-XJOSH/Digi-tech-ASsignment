97a8801cc4ab975b1b6b5ae5cdb8f57a291681e7
Jeremy <83259683+Jeremy-Tsai-310206-XJOSH@users.noreply.github.com>
2022-06-03 14:44:21 +1000

    Update Example.sql

--- Example.sql
+++ Example.sql
@@ -31,8 +31,10 @@ CREATE TABLE purchases (
 );
 
 INSERT INTO items(name) VALUES ("Computer"), ("Phones"), ("Headphones"),("Watches");
-INSERT INTO users(name) VALUES ("Bob"), ("Ben"), ("Jack"), ("Adam"), ("Josh");
+INSERT INTO users(name, password, email, phone, address, credit_card) VALUES ("Bob", "tigerbob123", "bobscot@hotmail.com", "0493285934
+", "1497 Hillside Street", "VISA, 4556004243430446, 7/2028, 441"), ("Ben", "benjamin125", "benboud@gmail.com", "0429384203", "3490 Pride Avenue", "MASTERCARD, 5148993138717226, 11/2025, 906"), ("Jack", "dragon943", "jackbox32@gmail.com", "0492385023", "197 Spruce Drive", "AMEX, 378427217548816, 3/2024, 180"), ("Adam", "adamname124", "adamthebest@hotmail.com", "0492587303", "1641 Lee Avenue", "VISA, 4916262269035284, 9/2023, 484"), ("Josh", "starwars012", "joshsmiths@hotmail.com", "0419342935", "1082 Charles Street", "VISA, 4916546246877523, 12/2026, 275");
 INSERT INTO purchases(user_id, item_id) VALUES (3, 3), (2, 3),(1, 1), (4,2),(2,4),(1,3),(3,2),(5,4) ;
+
 
 SELECT * FROM items;
 SELECT * FROM users;
